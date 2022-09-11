3d01695f16fa4b79531fcaeb81fddc17136477fa
Fatima Khan <xfatimax@Fatimas-MacBook-Air.local>
2022-09-11 01:14:51 -0400

    Button built and renders styles based on state

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,33 @@
 import React from "react";
+import classNames from "classnames";
 
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+   //return <><button>{props.children}</button>;</>;
+
+   // let buttonClass = "button";
+
+   // if (props.confirm) {
+   //   buttonClass += " button--confirm";
+   // }
+
+   // if (props.danger) {
+   //    buttonClass += " button--danger";
+   // }
+
+   const buttonClass = classNames("button", {
+      "button--confirm": props.confirm,
+      "button--danger": props.danger
+    });
+ 
+   return (
+      <button
+        className={buttonClass}
+        onClick={props.onClick}
+        disabled={props.disabled}
+      >
+        {props.children}
+      </button>
+    );
 }
